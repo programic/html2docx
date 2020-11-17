ccec62c2bf582ce59e9f76fc25432bb6e9ff832e
Rick Bongers <r.bongers@programic.com>
2020-11-17 11:23:17 +0100

    add custom styles

--- src/Docx.php
+++ src/Docx.php
@@ -35,24 +35,24 @@ class Docx
         $base_root = function_exists('url') ? url('/') : null;
         $base_path = function_exists('base_path') ? base_path() : null;
         $this->settings = array_merge([
-          // Required parameters:
-          'phpword_object' => &$this->phpWord, // Must be passed by reference.
-          'base_root' => $base_root, // Required for link elements - change it to your domain.
-          'base_path' => $base_path, // Path from base_root to whatever url your links are relative to.
-          // Optional parameters - showing the defaults if you don't set anything:
-          'current_style' => ['size' => '11'], // The PHPWord style on the top element
-          'parents' => [0 => 'body'], // Our parent is body.
-          'list_depth' => 0, // This is the current depth of any current list.
-          'context' => 'section', // Possible values - section, footer or header.
-          'pseudo_list' => true, // NOTE: Word lists not yet supported (TRUE is the only option at present).
-          'pseudo_list_indicator_font_name' => 'Wingdings', // Bullet indicator font.
-          'pseudo_list_indicator_font_size' => '7', // Bullet indicator size.
-          'pseudo_list_indicator_character' => 'l ', // Gives a circle bullet point with wingdings.
-          'table_allowed' => true, // Tables cannot be nested in PHPWord.
-          'treat_div_as_paragraph' => true, // If set to TRUE, each new div will trigger a new line in the Word document.
+            // Required parameters:
+            'phpword_object' => &$this->phpWord, // Must be passed by reference.
+            'base_root' => $base_root, // Required for link elements - change it to your domain.
+            'base_path' => $base_path, // Path from base_root to whatever url your links are relative to.
+            // Optional parameters - showing the defaults if you don't set anything:
+            'current_style' => ['size' => '11'], // The PHPWord style on the top element
+            'parents' => [0 => 'body'], // Our parent is body.
+            'list_depth' => 0, // This is the current depth of any current list.
+            'context' => 'section', // Possible values - section, footer or header.
+            'pseudo_list' => true, // NOTE: Word lists not yet supported (TRUE is the only option at present).
+            'pseudo_list_indicator_font_name' => 'Wingdings', // Bullet indicator font.
+            'pseudo_list_indicator_font_size' => '7', // Bullet indicator size.
+            'pseudo_list_indicator_character' => 'l ', // Gives a circle bullet point with wingdings.
+            'table_allowed' => true, // Tables cannot be nested in PHPWord.
+            'treat_div_as_paragraph' => true, // If set to TRUE, each new div will trigger a new line in the Word document.
 
-          // This is an array (the "style sheet")
-          'style_sheet' => array_merge($this->getStyles(), $this->styles),
+            // This is an array (the "style sheet")
+            'style_sheet' => $this->getStyles(),
         ], $settings);
     }
 
@@ -112,6 +112,15 @@ class Docx
         $this->dom = null;
     }
 
+    private function settings()
+    {
+        foreach ($this->settings['style_sheet'] as $type => $setting) {
+            if (isset($this->styles[$type])) {
+                $this->settings['style_sheet'][$type] = array_merge($setting, $this->styles[$type]);
+            }
+        }
+    }
+
     /**
      * @param $location
      * @param null $writerInterface
@@ -121,6 +130,7 @@ class Docx
 
     public function save($location, $writerInterface=null)
     {
+        $this->settings();
         $this->render();
 
         if ($this->writerInterface !== null) {
